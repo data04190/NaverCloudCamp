2ec44df8d892214a0bb8bb1633c0c553d4949ce9
data04190 <data04190@naver.com>
2023-06-05 15:43:21 +0900

    Classify methods according to their roles

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,40 +1,26 @@
 package bitcamp.myapp;
 
-// 코드 본문에서 사용할 클래스가 어떤 패키지의 클래스인지 지정한다.
-import java.util.Scanner;
+import bitcamp.myapp.handler.MemberHandler;
+import bitcamp.util.Prompt;
 
 public class App {
 
-  static final int MAX_SIZE = 100;
-
-  static Scanner scanner = new Scanner(System.in);
-  static int[] no = new int[MAX_SIZE];
-  static String[] name = new String[MAX_SIZE];
-  static String[] email = new String[MAX_SIZE];
-  static String[] password = new String[MAX_SIZE];
-  static char[] gender = new char[MAX_SIZE];
-
   public static void main(String[] args) {
-
-    // 키보드 스캐너 준비
-
-    int userId = 1;
-    int length = 0;
 
     printTitle();
 
     // 회원정보 등록
-    for (int i = 0; i < MAX_SIZE; i++) {
-      inputMember(i, userId);
-      length++;
+    while (MemberHandler.available()) {
+      MemberHandler.inputMember();
       if (!promptContinue()) {
         break;
       }
     }
 
-    printMembers(length);
+    MemberHandler.printMembers();
 
-    scanner.close();
+    Prompt.close();
+
   }
 
   static void printTitle() {
@@ -42,86 +28,12 @@ public class App {
     System.out.println("----------------------------------");
   }
 
-  static void inputMember(int i, int userId) {
-
-    System.out.print("이름? ");
-    name[i] = scanner.next();
-
-    System.out.print("이메일? ");
-    email[i] = scanner.next();
-
-    System.out.print("암호? ");
-    password[i] = scanner.next();
-
-    /*
-     * 성별?
-     * 
-     * 1. 남자
-     * 2. 여자
-     * > 1
-     * > 2
-     * > 3
-     * 무효한 번호입니다?
-     * 성별:
-     * 1. 남자
-     * 2. 여자
-     * >
-     * 
-     */
-
-    loop: while (true) {
-
-      System.out.println("성별: ");
-      System.out.println(" 1. 남자");
-      System.out.println(" 2. 여자");
-      System.out.print("> ");
-      String menuNo = scanner.next();
-      scanner.nextLine(); // 입력 값(token)을 읽고 난 후에 남아 있는 줄바꿈 코드를 제거한다.
-
-      // if (menuNo.equals("1")) {
-      // gender[i] = 'M';
-      // break;
-      // } else if (menuNo.equals("2")) {
-      // gender[i] = 'W';
-      // break;
-      // } else {
-      // System.out.println("무효한 번호입니다.");
-      // }
-
-      switch (menuNo) {
-        case "1":
-          gender[i] = 'M';
-          break loop;
-        case "2":
-          gender[i] = 'W';
-          break loop;
-        default:
-          System.out.println("무효한 번호입니다.");
-      }
-    }
-
-    no[i] = userId++;
-  }
-
   static boolean promptContinue() {
-    System.out.println("계속 하시겠습니까?(Y/n) ");
-    String response = scanner.nextLine();
+    String response = Prompt.inputString("계속 하시겠습니까?(Y/n) ");
     if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
       return false;
     }
     return true;
-  }
-
-  static void printMembers(int length) {
-
-    System.out.println("---------------------------------------");
-
-    System.out.println("번호, 이름, 이메일, 성별");
-    System.out.println("---------------------------------------");
-
-    for (int i = 0; i < length; i++) {
-      System.out.printf("%d, %s, %s, %c\n", no[i], name[i], email[i], gender[i]);
-    }
   }
 
 }
