678123af0df8afa390c64b3e64e66fff307ddc55
data04190 <data04190@naver.com>
2023-06-08 12:36:42 +0900

    Member 데이터타입 정의

--- myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
@@ -1,15 +1,12 @@
 package bitcamp.myapp.handler;
 
 import bitcamp.util.Prompt;
+import bitcamp.myapp.vo.Member;
 
 public class MemberHandler {
 
   static final int MAX_SIZE = 100;
-  static int[] no = new int[MAX_SIZE];
-  static String[] name = new String[MAX_SIZE];
-  static String[] email = new String[MAX_SIZE];
-  static String[] password = new String[MAX_SIZE];
-  static char[] gender = new char[MAX_SIZE];
+  static Member[] members = new Member[MAX_SIZE];
   static int userId = 1;
   static int length = 0;
 
@@ -22,13 +19,18 @@ public class MemberHandler {
       System.out.println("더이상 입력할 수 없습니다!");
       return;
     }
-    name[length] = Prompt.inputString("이름? ");
-    email[length] = Prompt.inputString("이메일? ");
-    password[length] = Prompt.inputString("암호? ");
-    gender[length] = inputGender((char)0);
-    
-    no[length] = userId++;
-    length++;
+
+    Member m = new Member();
+    m.name = Prompt.inputString("이름? ");
+    m.email = Prompt.inputString("이메일? ");
+    m.password = Prompt.inputString("암호? ");
+    m.gender = inputGender((char)0);
+    m.no = userId++;
+
+    //위에서 만든 Member 인스턴스의 주소를 잃어버리지 않게
+    //레퍼런스 배열에 담는다.
+    members[length++] = m;
+
   }
 
   public static void printMembers() {
@@ -38,9 +40,10 @@ public class MemberHandler {
     System.out.println("---------------------------------------");
 
     for (int i = 0; i < length; i++) {
+      Member m = members[i];
       System.out.printf("%d, %s, %s, %s\n",
-          no[i], name[i], email[i],
-          toGenderString(gender[i]));
+          m.no, m.name, m.email,
+          toGenderString(m.gender));
     }
   }
 
@@ -48,10 +51,11 @@ public class MemberHandler {
     String memberNo = Prompt.inputString("번호? ");
     // 입력 받은 번호를 가지고 배열에서 해당 넘버를 찾아야 한다.
     for (int i = 0; i < length; i++) {
-      if (no[i] == Integer.parseInt(memberNo)) {
-        System.out.printf("이름: %s\n", name[i]);
-        System.out.printf("이메일: %s\n", email[i]);
-        System.out.printf("성별: %s\n", toGenderString(gender[i]));
+      Member m = members[i];
+      if (m.no == Integer.parseInt(memberNo)) {
+        System.out.printf("이름: %s\n", m.name);
+        System.out.printf("이메일: %s\n", m.email);
+        System.out.printf("성별: %s\n", toGenderString(m.gender));
         // i번째 항목에 저장된 회원 정보 출력
         return;
       }
@@ -60,14 +64,15 @@ public class MemberHandler {
   }
 
   public static void updateMember() {
+
     String memberNo = Prompt.inputString("번호? ");
     for (int i = 0; i < length; i++) {
-      if (no[i] == Integer.parseInt(memberNo)) {
-
-        name[i] = Prompt.inputString("이름(" + name[i] + ")? ");
-        email[i] = Prompt.inputString("이메일(" + email[i] + ")? ");
-        password[i] = Prompt.inputString("새암호? ");
-        gender[i] = inputGender(gender[i]);
+      Member m = members[i];
+      if (m.no == Integer.parseInt(memberNo)) {
+        m.name = Prompt.inputString("이름(" + m.name + ")? ");
+        m.email = Prompt.inputString("이메일(" + m.email + ")? ");
+        m.password = Prompt.inputString("새암호? ");
+        m.gender = inputGender(m.gender);
         return;
       }
     }
@@ -114,25 +119,16 @@ public class MemberHandler {
     }
 
     for (int i = deletedIndex; i < length - 1; i++) {
-      no[i] = no[i + 1];
-      name[i] = name[i + 1];
-      email[i] = email[i + 1];
-      password[i] = password[i + 1];
-      gender[i] = gender[i + 1];
+      members[i] = members[i+1];
     }
 
-    no[length - 1] = 0;
-    name[length - 1] = null;
-    email[length - 1] = null;
-    password[length - 1] = null;
-    gender[length - 1] = (char) 0;
-
-    length--;
+    members[--length] = null;
   }
 
   private static int indexOf(int memberNo) {
     for (int i = 0; i < length; i++) {
-      if (no[i] == memberNo) {
+      Member m = members[i];
+      if (m.no == memberNo) {
         return i;
       }
     }
