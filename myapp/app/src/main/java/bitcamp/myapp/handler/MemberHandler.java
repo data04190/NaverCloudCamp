f305bf754135f700a01308061dc8fae4e8aa3766
data04190 <data04190@naver.com>
2023-06-12 16:03:52 +0900

    private, setter/getter 추가

--- myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
@@ -1,7 +1,7 @@
 package bitcamp.myapp.handler;
 
-import bitcamp.util.Prompt;
 import bitcamp.myapp.vo.Member;
+import bitcamp.util.Prompt;
 
 public class MemberHandler {
 
@@ -13,6 +13,7 @@ public class MemberHandler {
   static final char MALE = 'M';
   static final char FEMALE = 'W';
 
+
   public static void inputMember() {
 
     if (!available()) {
@@ -21,14 +22,14 @@ public class MemberHandler {
     }
 
     Member m = new Member();
-    m.name = Prompt.inputString("이름? ");
-    m.email = Prompt.inputString("이메일? ");
-    m.password = Prompt.inputString("암호? ");
-    m.gender = inputGender((char)0);
-    m.no = userId++;
+    m.setName(Prompt.inputString("이름? "));
+    m.setEmail(Prompt.inputString("이메일? "));
+    m.setPassword(Prompt.inputString("암호? "));
+    m.setGender(inputGender((char) 0));
+    m.setNo(userId++);
 
-    //위에서 만든 Member 인스턴스의 주소를 잃어버리지 않게
-    //레퍼런스 배열에 담는다.
+    // 위에서 만든 Member 인스턴스의 주소를 잃어버리지 않게
+    // 레퍼런스 배열에 담는다.
     members[length++] = m;
 
   }
@@ -41,9 +42,8 @@ public class MemberHandler {
 
     for (int i = 0; i < length; i++) {
       Member m = members[i];
-      System.out.printf("%d, %s, %s, %s\n",
-          m.no, m.name, m.email,
-          toGenderString(m.gender));
+      System.out.printf("%d, %s, %s, %s\n", m.getNo(), m.getName(), m.getEmail(),
+          toGenderString(m.getGender()));
     }
   }
 
@@ -52,10 +52,10 @@ public class MemberHandler {
     // 입력 받은 번호를 가지고 배열에서 해당 넘버를 찾아야 한다.
     for (int i = 0; i < length; i++) {
       Member m = members[i];
-      if (m.no == Integer.parseInt(memberNo)) {
-        System.out.printf("이름: %s\n", m.name);
-        System.out.printf("이메일: %s\n", m.email);
-        System.out.printf("성별: %s\n", toGenderString(m.gender));
+      if (m.getNo() == Integer.parseInt(memberNo)) {
+        System.out.printf("이름: %s\n", m.getName());
+        System.out.printf("이메일: %s\n", m.getEmail());
+        System.out.printf("성별: %s\n", toGenderString(m.getGender()));
         // i번째 항목에 저장된 회원 정보 출력
         return;
       }
@@ -68,11 +68,11 @@ public class MemberHandler {
     String memberNo = Prompt.inputString("번호? ");
     for (int i = 0; i < length; i++) {
       Member m = members[i];
-      if (m.no == Integer.parseInt(memberNo)) {
-        m.name = Prompt.inputString("이름(" + m.name + ")? ");
-        m.email = Prompt.inputString("이메일(" + m.email + ")? ");
-        m.password = Prompt.inputString("새암호? ");
-        m.gender = inputGender(m.gender);
+      if (m.getNo() == Integer.parseInt(memberNo)) {
+        m.setName(Prompt.inputString("이름(" + m.getName() + ")? "));
+        m.setEmail(Prompt.inputString("이메일(" + m.getEmail() + ")? "));
+        m.setPassword(Prompt.inputString("새암호? "));
+        m.setGender(inputGender(m.getGender()));
         return;
       }
     }
@@ -91,15 +91,12 @@ public class MemberHandler {
     } else {
       label = String.format("성별(%s)?\n", toGenderString(gender));
     }
-    loop: while (true) {
-      String menuNo = Prompt.inputString(label +
-          " 1. 남자\n" +
-          " 2. 여자\n" +
-          "> ");
+    while (true) {
+      String menuNo = Prompt.inputString(label + " 1. 남자\n" + " 2. 여자\n" + "> ");
 
       switch (menuNo) {
         case "1":
-          return MALE;  
+          return MALE;
         case "2":
           return FEMALE;
         default:
@@ -119,7 +116,7 @@ public class MemberHandler {
     }
 
     for (int i = deletedIndex; i < length - 1; i++) {
-      members[i] = members[i+1];
+      members[i] = members[i + 1];
     }
 
     members[--length] = null;
@@ -128,7 +125,7 @@ public class MemberHandler {
   private static int indexOf(int memberNo) {
     for (int i = 0; i < length; i++) {
       Member m = members[i];
-      if (m.no == memberNo) {
+      if (m.getNo() == memberNo) {
         return i;
       }
     }
