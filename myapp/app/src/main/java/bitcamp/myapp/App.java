58ad385e19ee7866f727533f550ecdc5d4f9f4fc
data04190 <data04190@naver.com>
2023-06-28 14:29:45 +0900

    바이너리 파일 입출력- load, save 함수 생성

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,5 +1,7 @@
 package bitcamp.myapp;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import bitcamp.myapp.handler.BoardAddListener;
@@ -7,9 +9,6 @@ import bitcamp.myapp.handler.BoardDeleteListener;
 import bitcamp.myapp.handler.BoardDetailListener;
 import bitcamp.myapp.handler.BoardListListener;
 import bitcamp.myapp.handler.BoardUpdateListener;
-import bitcamp.myapp.handler.FooterListener;
-import bitcamp.myapp.handler.HeaderListener;
-import bitcamp.myapp.handler.HelloListener;
 import bitcamp.myapp.handler.MemberAddListener;
 import bitcamp.myapp.handler.MemberDeleteListener;
 import bitcamp.myapp.handler.MemberDetailListener;
@@ -23,16 +22,51 @@ import bitcamp.util.MenuGroup;
 
 public class App {
 
+  ArrayList<Member> memberList = new ArrayList<>();
+  LinkedList<Board> boardList = new LinkedList<>();
+  LinkedList<Board> readingList = new LinkedList<>();
+  BreadcrumbPrompt prompt = new BreadcrumbPrompt();
+
+  MenuGroup mainMenu = new MenuGroup("메인");
+
+  public App() {
+    prepareMenu();
+
+  }
+
   public static void main(String[] args) {
+    new App().execute();
+  }
 
-    ArrayList<Member> memberList = new ArrayList<>();
-    LinkedList<Board> boardList = new LinkedList<>();
-    LinkedList<Board> readingList = new LinkedList<>();
+  static void printTitle() {
+    System.out.println("나의 목록 관리 시스템");
+    System.out.println("----------------------------------");
+  }
 
-    BreadcrumbPrompt prompt = new BreadcrumbPrompt();
+  public void execute() {
+    printTitle();
 
-    MenuGroup mainMenu = new MenuGroup("메인");
+    loadData();
+    mainMenu.execute(prompt);
+    saveData();
+    prompt.close();
 
+  }
+
+  private void loadData() {
+    loadMember();
+    loadBoard();
+    // loadReading();
+  }
+
+  private void saveData() {
+    saveMember();
+    saveBoard();
+    // saveReading();
+
+  }
+
+  private void prepareMenu() {
     MenuGroup memberMenu = new MenuGroup("회원");
     memberMenu.add(new Menu("등록", new MemberAddListener(memberList)));
     memberMenu.add(new Menu("목록", new MemberListListener(memberList)));
@@ -56,22 +90,194 @@ public class App {
     readingMenu.add(new Menu("변경", new BoardUpdateListener(readingList)));
     readingMenu.add(new Menu("삭제", new BoardDeleteListener(readingList)));
     mainMenu.add(readingMenu);
-
-    Menu helloMenu = new Menu("안녕!");
-    helloMenu.addActionListener(new HeaderListener());
-    helloMenu.addActionListener(new HelloListener());
-    helloMenu.addActionListener(new FooterListener());
-    mainMenu.add(helloMenu);
-
-    printTitle();
-
-    mainMenu.execute(prompt);
-
-    prompt.close();
   }
 
-  static void printTitle() {
-    System.out.println("나의 목록 관리 시스템");
-    System.out.println("----------------------------------");
+  private void loadMember() {
+    try {
+      FileInputStream in = new FileInputStream("member.data");
+      int size = in.read() << 8;
+      size |= in.read();
+
+      byte[] buf = new byte[1000];
+
+
+      for (int i = 0; i < size; i++) {
+        Member member = new Member();
+        member.setNo(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
+
+        int length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        member.setName(new String(buf, 0, length, "UTF-8"));
+
+        length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        member.setEmail(new String(buf, 0, length, "UTF-8"));
+
+        length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        member.setPassword(new String(buf, 0, length, "UTF-8"));
+
+        member.setGender((char) (in.read() << 8 | in.read()));
+
+        memberList.add(member);
+      }
+
+      in.close();
+
+    } catch (Exception e) {
+      System.out.println("회원 정보를 읽는 중 오류 발생!");
+    }
   }
+
+  private void saveMember() {
+    try {
+      FileOutputStream out = new FileOutputStream("member.data");
+
+      // 저장할 데이터의 개수를 먼저 출력한다.
+      int size = memberList.size();
+      out.write(size >> 8);
+      out.write(size);
+
+      for (Member member : memberList) {
+        int no = member.getNo();
+        out.write(no >> 24);
+        out.write(no >> 16);
+        out.write(no >> 8);
+        out.write(no);
+
+        byte[] bytes = member.getName().getBytes("UTF-8");
+        // 출력할 바이트 개수를 2바이트로 표시한다.
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+
+        out.write(bytes);
+
+        // 문자열의 바이트를 출력한다.
+        bytes = member.getEmail().getBytes("UTF-8");
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        bytes = member.getPassword().getBytes("UTF-8");
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        char gender = member.getGender();
+        out.write(gender >> 8);
+        out.write(gender);
+
+      }
+      out.close();
+    } catch (Exception e) {
+      System.out.println("회원 정보를 저장하는 중 오류 발생!");
+    }
+  }
+
+  private void loadBoard() {
+    try {
+      FileInputStream in = new FileInputStream("board.data");
+      int size = in.read() << 8;
+      size |= in.read();
+
+      byte[] buf = new byte[1000];
+
+      for (int i = 0; i < size; i++) {
+        Board board = new Board();
+        board.setNo(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
+
+        int length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        board.setTitle(new String(buf, 0, length, "UTF-8"));
+
+        length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        board.setContent(new String(buf, 0, length, "UTF-8"));
+
+        length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        board.setWriter(new String(buf, 0, length, "UTF-8"));
+
+        length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        board.setPassword(new String(buf, 0, length, "UTF-8"));
+
+        board.setViewCount(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
+
+        board.setCreatedDate((long) in.read() << 56 | (long) in.read() << 48
+            | (long) in.read() << 40 | (long) in.read() << 32 | (long) in.read() << 24
+            | (long) in.read() << 16 | (long) in.read() << 8 | in.read());
+
+        boardList.add(board);
+      }
+
+      in.close();
+
+    } catch (Exception e) {
+      System.out.println("회원 정보를 읽는 중 오류 발생!");
+    }
+  }
+
+  private void saveBoard() {
+    try {
+      FileOutputStream out = new FileOutputStream("board.data");
+
+      // 저장할 데이터의 개수를 먼저 출력한다.
+      int size = boardList.size();
+      out.write(size >> 8);
+      out.write(size);
+
+      for (Board board : boardList) {
+        int no = board.getNo();
+        out.write(no >> 24);
+        out.write(no >> 16);
+        out.write(no >> 8);
+        out.write(no);
+
+        byte[] bytes = board.getTitle().getBytes("UTF-8");
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+
+        bytes = board.getContent().getBytes("UTF-8");
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        bytes = board.getWriter().getBytes("UTF-8");
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        bytes = board.getPassword().getBytes("UTF-8");
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        int viewCount = board.getViewCount();
+        out.write(viewCount >> 24);
+        out.write(viewCount >> 16);
+        out.write(viewCount >> 8);
+        out.write(viewCount);
+
+        long createdDate = board.getCreatedDate();
+        out.write((int) (createdDate >> 56));
+        out.write((int) (createdDate >> 48));
+        out.write((int) (createdDate >> 40));
+        out.write((int) (createdDate >> 32));
+        out.write((int) (createdDate >> 24));
+        out.write((int) (createdDate >> 16));
+        out.write((int) (createdDate >> 8));
+        out.write((int) createdDate);
+      }
+      out.close();
+
+    } catch (Exception e) {
+      System.out.println("게시글 정보를 저장하는 중 오류 발생!");
+    }
+  }
+
+
+
 }
--- myapp/app/src/main/java/bitcamp/myapp/handler/AbstractBoardListener.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/AbstractBoardListener.java
@@ -23,3 +23,4 @@ public abstract class AbstractBoardListener implements ActionListener {
   }
 
 }
+
