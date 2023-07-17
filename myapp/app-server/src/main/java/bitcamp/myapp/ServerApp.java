69cf403dcb90622747326602691106e5ded7c7e9
data04190 <data04190@naver.com>
2023-07-17 09:48:42 +0900

    Thread Pool 구현

--- myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -12,6 +12,8 @@ import bitcamp.myapp.dao.BoardListDao;
 import bitcamp.myapp.dao.MemberListDao;
 import bitcamp.net.RequestEntity;
 import bitcamp.net.ResponseEntity;
+import bitcamp.util.ManagedThread;
+import bitcamp.util.ThreadPool;
 
 public class ServerApp {
 
@@ -19,6 +21,9 @@ public class ServerApp {
   ServerSocket serverSocket;
 
   HashMap<String,Object> daoMap = new HashMap<>();
+
+  // 스레드를 리턴해 줄 스레드풀 준비
+  ThreadPool threadPool = new ThreadPool();
 
   public ServerApp(int port) throws Exception {
     this.port = port;
@@ -50,7 +55,9 @@ public class ServerApp {
     System.out.println("서버 실행 중...");
 
     while (true) {
-      processRequest(serverSocket.accept());
+      Socket socket = serverSocket.accept();
+      ManagedThread t = threadPool.getResource();
+      t.setJob(() ->processRequest(socket));
     }
   }
 
@@ -82,6 +89,10 @@ public class ServerApp {
       System.out.printf("%s:%s 클라이언트가 접속했음!\n",
           socketAddress.getHostString(),
           socketAddress.getPort());
+
+      // 스레드풀이 새 스레드를 만드는 것을 테스트하기 위함.
+      // => 스레드풀에 스레드가 없을 때 새 스레드를 만들 것이다.
+      Thread.sleep(10000);
 
       // 클라이언트 요청을 반복해서 처리하지 않는다.
       // => 접속 -> 요청 -> 실행 -> 응답 -> 연결 끊기
@@ -130,7 +141,6 @@ public class ServerApp {
       System.out.println(e.getMessage());
     }
   }
-
 }
 
 
