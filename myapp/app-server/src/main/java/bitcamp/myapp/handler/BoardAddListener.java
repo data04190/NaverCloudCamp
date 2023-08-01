1adf6715b40c9d380219c40d9317b72f78d66239
data04190 <data04190@naver.com>
2023-08-01 19:16:44 +0900

    20230801 - 웹 애플리케이션 서버 구조 전환

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardAddListener.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardAddListener.java
@@ -1,21 +1,23 @@
 package bitcamp.myapp.handler;
 
 import java.io.IOException;
+import org.apache.ibatis.session.SqlSessionFactory;
 import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.Member;
 import bitcamp.util.ActionListener;
 import bitcamp.util.BreadcrumbPrompt;
-import bitcamp.util.DataSource;
+import bitcamp.util.Component;
 
+@Component("/board/add")
 public class BoardAddListener implements ActionListener {
 
   BoardDao boardDao;
-  DataSource ds;
+  SqlSessionFactory sqlSessionFactory;
 
-  public BoardAddListener(BoardDao boardDao, DataSource ds) {
+  public BoardAddListener(BoardDao boardDao, SqlSessionFactory sqlSessionFactory) {
     this.boardDao = boardDao;
-    this.ds = ds;
+    this.sqlSessionFactory = sqlSessionFactory;
   }
 
   @Override
@@ -24,27 +26,26 @@ public class BoardAddListener implements ActionListener {
     board.setTitle(prompt.inputString("제목? "));
     board.setContent(prompt.inputString("내용? "));
     board.setWriter((Member) prompt.getAttribute("loginUser"));
+    board.setCategory(Integer.parseInt((String)prompt.getAttribute("category")));
 
     try {
       boardDao.insert(board);
-      Thread.sleep(5000);
+      sqlSessionFactory.openSession(false).commit();
 
-      boardDao.insert(board);
-      Thread.sleep(5000);
-
-      boardDao.insert(board);
-      Thread.sleep(5000);
-
-      ds.getConnection().commit();
     } catch (Exception e) {
-      try {
-        ds.getConnection().rollback();
-      } catch (Exception e2) {
-      }
+      sqlSessionFactory.openSession(false).rollback();
       throw new RuntimeException(e);
     }
-
   }
 }
+
+
+
+
+
+
+
+
+
 
 
