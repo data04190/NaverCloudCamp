c415b9e9f5f1f805c6b3804e48c14a469bceef24
data04190 <data04190@naver.com>
2023-08-10 15:23:17 +0900

    20230810 - DB모델링: ER-Diagram 작성

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/InitServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/InitServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -14,15 +15,12 @@ import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.dao.MySQLBoardDao;
 import bitcamp.myapp.dao.MySQLMemberDao;
-import bitcamp.util.AbstractServlet;
 import bitcamp.util.SqlSessionFactoryProxy;
 
-@WebServlet(
-    value="/init",
-    loadOnStartup = 1
-    )
-public class InitServlet extends AbstractServlet {
+@WebServlet(value = "/init", loadOnStartup = 1)
+public class InitServlet extends HttpServlet {
 
+  private static final long serialVersionUID = 1L;
   public static SqlSessionFactory sqlSessionFactory;
   public static BoardDao boardDao;
   public static MemberDao memberDao;
@@ -33,9 +31,8 @@ public class InitServlet extends AbstractServlet {
     System.out.println("InitServlet.init() 호출됨!");
 
     try {
-      sqlSessionFactory = new SqlSessionFactoryProxy(
-          new SqlSessionFactoryBuilder().build(
-              Resources.getResourceAsStream("bitcamp/myapp/config/mybatis-config.xml")));
+      sqlSessionFactory = new SqlSessionFactoryProxy(new SqlSessionFactoryBuilder()
+          .build(Resources.getResourceAsStream("bitcamp/myapp/config/mybatis-config.xml")));
 
       boardDao = new MySQLBoardDao(sqlSessionFactory);
       memberDao = new MySQLMemberDao(sqlSessionFactory);
