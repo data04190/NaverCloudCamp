0e549dd48ac500b74f83018c286dc1bbd5684b9a
data04190 <data04190@naver.com>
2023-08-30 17:11:27 +0900

    20230830 - Spring IoC 컨테이너를 이용하여 페이지 컨트롤러 관리

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/HomeController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/HomeController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/index.jsp")
+@WebServlet("/")
 public class HomeController extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
