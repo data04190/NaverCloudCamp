442848dcacb706d3f2fcea2df9cf16c2245c0b11
data04190 <data04190@naver.com>
2023-08-30 15:45:25 +0900

    20230830 - 페이지 컨트롤러를 POJO 클래스로 변경

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/LoginController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/LoginController.java
@@ -3,16 +3,17 @@ package bitcamp.myapp.controller;
 import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.vo.Member;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 
 public class LoginController implements PageController {
+
+  MemberDao memberDao;
+
+  public LoginController(MemberDao memberDao) {
+    this.memberDao = memberDao;
+  }
 
   @Override
   public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -33,13 +34,12 @@ public class LoginController implements PageController {
       response.addCookie(cookie);
     }
 
-    MemberDao memberDao = (MemberDao) request.getServletContext().getAttribute("memberDao");
     Member loginUser = memberDao.findByEmailAndPassword(m);
     if (loginUser == null) {
       request.setAttribute("refresh", "2;url=/app/auth/login");
       throw new Exception("회원 정보가 일치하지 않습니다.");
-
     }
+
     request.getSession().setAttribute("loginUser", loginUser);
     return "redirect:/";
   }
--- myapp/app-server/src/main/java/bitcamp/myapp/controller/LogoutController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/LogoutController.java
@@ -1,15 +1,9 @@
 package bitcamp.myapp.controller;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 
 public class LogoutController implements PageController {
-
   @Override
   public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
     request.getSession().invalidate();
